30d0682af257fb821505320c561fede0949e6a73
sufiyaneng <71214149+sufiyaneng@users.noreply.github.com>
2021-06-30 13:41:41 +0530

    update library
    update class libraries

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -3,6 +3,10 @@ package com.techprimers.aws;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import package br.com.SpringApp.SpringApp;
+import package br.com.SpringApp.SpringApp.controller;
+import package br.com.SpringApp.SpringApp.model;
+
 
 @RequestMapping("/hello")
 @RestController
